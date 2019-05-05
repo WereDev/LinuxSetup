66db0e8b54284c63ace8a5c5e33a908318fcd083
weredev <danielm@weredev.com>
2019-05-05 14:00:51 -0500

    more tweaks

--- InstallCodingPlatform.sh
+++ InstallCodingPlatform.sh
@@ -4,9 +4,9 @@
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo add-apt-repository universe
-sudo apt-get -y install apt-transport-https
-sudo apt-get update
-sudo apt-get -y install dotnet-sdk-2.2
+sudo apt -y install apt-transport-https
+sudo apt update
+sudo apt -y install dotnet-sdk-2.2
 
 # Install VS Code
 sudo snap install code --classic
@@ -17,6 +17,7 @@ sudo apt -y install git
 # Install GitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
+sudo apt --fix-borken -y install
 
 echo "DOTNET_CLI_TELEMETRY_OPTOUT=1" | sudo tee -a /etc/environment
 
