1ee8e7b355daf2413f53637dae6ae0a32ae17b68
weredev <danielm@weredev.com>
2019-05-27 10:41:27 -0500

    fixing bash keymapping and adding other software installs

--- BashKeymapping.sh
+++ BashKeymapping.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 echo "TAB: menu-complete" | sudo tee -a ~/.inputrc
-echo "bind TAB: menu-complete" | sudo tee -a ~/.bashrc
---
+++ InstallOtherSoftware.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/Downloads
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
