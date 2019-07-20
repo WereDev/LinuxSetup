e57fb630d59fb4c6691fbb6a1d3e9d9420bbb430
WereDev <WereDev@users.noreply.github.com>
2019-07-20 16:16:44 -0500

    adding Vivaldi

--- InstallOtherSoftware.sh
+++ InstallOtherSoftware.sh
@@ -4,3 +4,9 @@ cd ~/Downloads
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Vivaldi
+wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
+sudo apt update
+sudo apt -y install vivaldi-stable
