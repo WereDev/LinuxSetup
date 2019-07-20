1ed5bd2df891c494b2fc72bc253ad1fbc1137416
Weredev <danielm@weredev.com>
2019-07-20 16:21:15 -0500

    Adding vscode extensions list

---
+++ VSCodeExtensions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install vscode extensions
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension liviuschera.noctis
+code --install-extension ms-vscode.csharp
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension Yummygum.city-lights-icon-vsc
+code --install-extension yummygum.city-lights-theme
