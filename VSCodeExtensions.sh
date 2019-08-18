812ad77faac34a0f62e37d120cc0fed639424b4c
WereDev <WereDev@users.noreply.github.com>
2019-08-18 18:22:33 -0500

    Update VSCodeExtensions.sh

--- VSCodeExtensions.sh
+++ VSCodeExtensions.sh
@@ -3,9 +3,11 @@
 # install vscode extensions
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension eamodio.gitlens
+code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.dotnet-test-explorer
 code --install-extension liviuschera.noctis
 code --install-extension ms-vscode.csharp
-code --install-extension vscode-icons-team.vscode-icons
-code --install-extension Yummygum.city-lights-icon-vsc
-code --install-extension yummygum.city-lights-theme
+code --install-extension ms-vscode.vs-keybindings
+code --install-extension PKief.material-icon-theme
+code --install-extension VisualStudioExptTeam.vscodeintellicode
