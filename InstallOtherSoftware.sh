d2ca29be728b5faa89fa212ef1d6465f451758e5
WereDev <WereDev@users.noreply.github.com>
2019-10-19 15:21:07 -0500

    Update InstallOtherSoftware.sh

--- InstallOtherSoftware.sh
+++ InstallOtherSoftware.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd ~/Downloads
 
-# Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 # Vivaldi
 wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
