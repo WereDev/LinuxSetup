3e8eafda98605656265b2b3c3d23da43270b6c81
WereDev <WereDev@users.noreply.github.com>
2019-10-19 15:41:27 -0500

    Create SystemConfig.sh

---
+++ SystemConfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Enabling multi-finger gestures
+# Assumes libinput
+sudo gpasswd -a $USER input
+sudo apt-get install -y xdotool wmctrl
+sudo apt-get install -y libinput-tools
+git clone https://github.com/bulletmark/libinput-gestures.git
+cd libinput-gestures
+sudo make install
+libinput-gestures-setup autostart
+libinput-gestures-setup start
+# will need to reboot at this point
