db5d098b2a622271cf4b8dcf006aaea912b65852
WereDev <WereDev@users.noreply.github.com>
2020-01-13 07:53:59 -0600

    removing smartgit and yarn, adding sqlitebrowser

--- InstallCodingPlatform.sh
+++ InstallCodingPlatform.sh
@@ -6,7 +6,7 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo add-apt-repository universe
 sudo apt -y install apt-transport-https
 sudo apt update
-sudo apt -y install dotnet-sdk-2.2
+sudo apt -y install dotnet-sdk-3.1
 echo "DOTNET_CLI_TELEMETRY_OPTOUT=1" | sudo tee -a /etc/environment
 
 # Install VS Code
@@ -17,14 +17,8 @@ echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo
 # Install Git
 sudo apt -y install git
 
-# Install SmartGit
-wget https://www.syntevo.com/downloads/smartgit/smartgit-19_1_4.deb
-sudo dpkg -i smartgit-19_1_4.deb
-
 # Install NodeJs
 sudo snap install node --classic --channel=10
 
-# Install Yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt -y install yarn
+# Install SqliteBrowser
+sudo apt -y install sqlitebrowser
