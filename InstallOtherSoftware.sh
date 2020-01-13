ef64ba09142ce9f138d57fb21d01748ee92cb01a
WereDev <WereDev@users.noreply.github.com>
2020-01-13 07:54:23 -0600

    removing vmware

--- InstallOtherSoftware.sh
+++ InstallOtherSoftware.sh
@@ -6,8 +6,3 @@ wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key ad
 sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
 sudo apt update
 sudo apt -y install vivaldi-stable
-
-#VMware Player
-wget https://download3.vmware.com/software/player/file/VMware-Player-15.5.0-14665864.x86_64.bundle
-chmod +x VMware-Player-15.5.0-14665864.x86_64.bundle
-sudo ./VMware-Player-15.5.0-14665864.x86_64.bundle
