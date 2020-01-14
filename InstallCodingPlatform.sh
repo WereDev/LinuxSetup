fb0ad3eaca9213b07761cc239847160f46e6cbd5
WereDev <WereDev@users.noreply.github.com>
2020-01-13 19:35:27 -0600

    update max file watch

--- InstallCodingPlatform.sh
+++ InstallCodingPlatform.sh
@@ -13,6 +13,7 @@ echo "DOTNET_CLI_TELEMETRY_OPTOUT=1" | sudo tee -a /etc/environment
 sudo apt install snapd
 sudo snap install code --classic
 echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 # Install Git
 sudo apt -y install git
