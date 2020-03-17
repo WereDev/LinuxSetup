ed0c9723c2c91ab7740e02d89f5da240e38093d0
WereDev <WereDev@users.noreply.github.com>
2020-03-17 13:25:40 -0500

    Update InstallOtherSoftware.sh

--- InstallOtherSoftware.sh
+++ InstallOtherSoftware.sh
@@ -2,7 +2,8 @@
 cd ~/Downloads
 
 # Vivaldi
-wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
-sudo apt update
-sudo apt -y install vivaldi-stable
+echo "echo deb http://repo.vivaldi.com/stable/deb/ stable main > /etc/apt/sources.list.d/vivaldi.list" | sudo sh
+curl http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
+sudo apt-get update
+sudo apt-get install -y vivaldi-stable
