bf53b930501ef7d98d586a957578313acfd8e035
WereDev <WereDev@users.noreply.github.com>
2020-03-17 16:29:43 -0500

    Update VSCodeExtensions.sh

--- VSCode/VSCodeExtensions.sh
+++ VSCode/VSCodeExtensions.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # install vscode extensions
+code --install-extension alexcvzz.vscode-sqlite
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension eamodio.gitlens
 code --install-extension emmanuelbeziat.vscode-great-icons
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.dotnet-test-explorer
+code --install-extension hangxingliu.vscode-systemd-support
 code --install-extension liviuschera.noctis
-code --install-extension ms-vscode.csharp
+code --install-extension ms-dotnettools.csharp
 code --install-extension ms-vscode.vs-keybindings
+code --install-extension Reptarsrage.vscode-manage-user-secrets
 code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension william-voyek.vscode-nginx
+code --install-extension yzhang.markdown-all-in-one
