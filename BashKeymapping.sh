ba9bb626355dfcef190a58a0e31b930eb2cebe14
WereDev <WereDev@users.noreply.github.com>
2020-03-19 20:05:12 -0500

    Update BashKeymapping.sh

--- BashKeymapping.sh
+++ BashKeymapping.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# Menu Completion
 echo "TAB: menu-complete" | sudo tee -a ~/.inputrc
+
+# Powerline
+sudo apt install -y powerline
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./fonts/install.sh
+cd ..
+rm -rf fonts
+
