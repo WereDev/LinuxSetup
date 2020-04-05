f3af8d0fb3ee7fc6aeba92815b2767864ab95708
WereDev <WereDev@users.noreply.github.com>
2020-04-05 11:45:51 -0500

    Update SystemConfig.sh
    adding TLP

--- SystemConfig.sh
+++ SystemConfig.sh
@@ -5,13 +5,19 @@ sudo apt install -y gnome-tweaks
 sudo apt install -y gnome-shell-extensions
 sudo apt install -y chrome-gnome-shell
 
-#Top Tray Icons
+# Top Tray Icons
 cd ~/.local/share/gnome-shell/extensions
 wget https://extensions.gnome.org/extension-data/TopIcons-gitphocean.net.v2.shell-extension.zip
 unzip ./TopIcons-gitphocean.net.v2.shell-extension.zip -d ./TopIcons-git@phocean.net
 rm TopIcons-gitphocean.net.v2.shell-extension.zip
 cd ~
 
+# Power Management
+# https://tipsonubuntu.com/2018/11/18/quick-tip-improve-battery-life-ubuntu-18-04-higher/
+sudo apt -y install tlp
+sudo add-apt-repository -y ppa:linuxuprising/apps
+sudo apt update
+sudo apt -y install tlpui
 
 # Enabling multi-finger gestures
 # Assumes libinput
