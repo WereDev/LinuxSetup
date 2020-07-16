bb7680d1f2095f531de44b76c0ad999370fa8728
WereDev <WereDev@users.noreply.github.com>
2020-07-15 20:17:07 -0500

    Update InstallCodingPlatform.sh

--- InstallCodingPlatform.sh
+++ InstallCodingPlatform.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Making sure we have all the supporting bits
-sudo apt install -y software-properties-common apt-transport-https wget snapd
+sudo apt install -y software-properties-common apt-transport-https wget
 
 # Install DotNet Core
 UBUNTU_VERSION=$(lsb_release -r -s)
 wget -q https://packages.microsoft.com/config/ubuntu/$UBUNTU_VERSION/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-sudo add-apt-repository universe
-sudo apt -y install apt-transport-https
 sudo apt update
 sudo apt -y install dotnet-sdk-3.1
 echo "DOTNET_CLI_TELEMETRY_OPTOUT=1" | sudo tee -a /etc/environment
 rm packages-microsoft-prod.deb
 
 # Install VS Code
-sudo snap install code --classic
+wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i vscode.deb
+rm vscode.deb
 echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 chmod +x $setupFolder/VSCode/VSCodeExtensions.sh
