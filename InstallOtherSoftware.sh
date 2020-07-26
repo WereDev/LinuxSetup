3e6c75c0c84c5cb39ebf3f7d37974eef88a0be5d
WereDev <github@weredev.com>
2020-07-26 12:35:46 -0500

    more software and some gnome themes

--- InstallOtherSoftware.sh
+++ InstallOtherSoftware.sh
@@ -2,7 +2,7 @@
 
 # Making sure we have all the supporting bits
 sudo apt install -y software-properties-common apt-transport-https wget flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Sublime
 flatpak install -y flathub com.sublimetext.three
@@ -15,3 +15,9 @@ flatpak install -y flathub org.inkscape.Inkscape
 
 # Master PDF
 flatpak install -y flathub net.codeindustry.MasterPDFEditor
+
+# Discord
+flatpak install -y flathub com.discordapp.Discord
+
+# Slack
+flatpak install -y flathub com.slack.Slack
---
+++ InstallThemes.sh
@@ -0,0 +1,3 @@
+
+sudo apt install -y arc-theme
+sudo apt install -y breeze-cursor-theme
