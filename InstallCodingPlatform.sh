254e88236694542b1650260522b4994bc288f606
WereDev <github@weredev.com>
2020-12-12 17:41:05 -0600

    going back to vscode, adding my new vscode extension

--- InstallCodingPlatform.sh
+++ InstallCodingPlatform.sh
@@ -13,15 +13,15 @@ sudo apt -y install dotnet-sdk-3.1
 echo "DOTNET_CLI_TELEMETRY_OPTOUT=1" | sudo tee -a /etc/environment
 rm packages-microsoft-prod.deb
 
-# Install VS Codium
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
-echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list 
-sudo apt update && sudo apt install codium 
+# Install VS Code
+wget -O vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i vscode.deb
+rm vscode.deb
 echo fs.inotify.max_user_instances=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 chmod +x ./VSCode/VSCodeExtensions.sh
 ./VSCode/VSCodeExtensions.sh
-cp ./VSCode/VSCodeSettings.json ~/.config/VSCodium/User/settings.json
+cp ./VSCode/VSCodeSettings.json ~/.config/Code/User/settings.json
 
 # Install Git
 sudo apt -y install git
--- VSCode/VSCodeExtensions.sh
+++ VSCode/VSCodeExtensions.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# install vscodium extensions
-codium --install-extension alexcvzz.vscode-sqlite
-codium --install-extension CoenraadS.bracket-pair-colorizer
-codium --install-extension eamodio.gitlens
-codium --install-extension emmanuelbeziat.vscode-great-icons
-codium --install-extension formulahendry.dotnet-test-explorer
-codium --install-extension liviuschera.noctis
-codium --install-extension ms-dotnettools.csharp
-codium --install-extension ms-vscode.vs-keybindings
-codium --install-extension Reptarsrage.vscode-manage-user-secrets
+# install vscode extensions
+# code --install-extension alexcvzz.vscode-sqlite
+code --install-extension CoenraadS.bracket-pair-colorizer
+# code --install-extension eamodio.gitlens
+code --install-extension emmanuelbeziat.vscode-great-icons
+code --install-extension formulahendry.dotnet-test-explorer
+code --install-extension liviuschera.noctis
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-vscode.vs-keybindings
+code --install-extension Reptarsrage.vscode-manage-user-secrets
+code --install-extension weredev.copyrawvalue
